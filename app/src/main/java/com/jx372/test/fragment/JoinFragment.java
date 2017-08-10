2dfe86ba2906149338719d6f27d811e164c2e296
Dell <naver@naver.com>
2017-08-10 18:29:02 +0900

    login

--- app/src/main/java/com/jx372/test/fragment/JoinFragment.java
+++ app/src/main/java/com/jx372/test/fragment/JoinFragment.java
@@ -16,5 +16,9 @@ public class JoinFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_join, null);
+
+  
+
     }
+
 }
